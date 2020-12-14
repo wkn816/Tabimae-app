f99fe1720642278509478580fbffe133a7906893
wkn816 <wkn816@gmail.com>
2020-12-14 17:49:16 +0900

    addユーザー新規登録情報をrailsAPIに紐付ける

--- tabimae-web/nuxt.config.js
+++ tabimae-web/nuxt.config.js
@@ -43,6 +43,8 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
+    // '@nuxtjs/axios',
+
   ],
 
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)

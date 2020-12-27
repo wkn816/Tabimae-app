3a00827571ecc37f4262535f74844a3e93a13622
wkn816 <wkn816@gmail.com>
2020-12-27 18:00:51 +0900

    fix/nokogiriエラー脱却修後正ログイン状態保持まで

--- tabimae-web/plugins/authCheck.js
+++ tabimae-web/plugins/authCheck.js
@@ -1,15 +1,19 @@
-import firebase from "@/plugins/firebase"
-import axios from "@/plugins/axios"
+import firebase from "@/plugins/firebase";
+import axios from "@/plugins/axios";
 
 const authCheck = ({ store, redirect }) => {
     firebase.auth().onAuthStateChanged(async user => {
-        if (user) {
-            const { data } = await axios.get(`/v1/users?uid=${user.uid}`)
-            store.commit("setUser", data)
-        } else {
-            store.commit("setUser", null)
-        }
+    if (user) {
+    const { data } = await axios.get('/v1/users', {
+        params: {
+            uid: user.uid,
+        },
     });
-}
+    store.commit("auth/setUser", data)
+    } else {
+    store.commit("auth/setUser", null)
+    }
+    });
+};
 
 export default authCheck;

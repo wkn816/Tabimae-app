1d77151a7fc31c0cfab06c6855da64a796617ef3
wkn816 <wkn816@gmail.com>
2020-12-27 18:46:36 +0900

    add-travel_serializer.rb追記

--- tabimae-api/app/serializers/travel_serializer.rb
+++ tabimae-api/app/serializers/travel_serializer.rb
@@ -1,3 +1,8 @@
 class TravelSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :transpor, :name, :user_id, :username
+  belongs_to :user
+
+  def username
+    object.user.name
+  end
 end

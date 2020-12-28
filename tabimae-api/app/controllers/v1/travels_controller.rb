1d01a2572f3eaf4bffecae5b8df106f7336c3fc3
wkn816 <wkn816@gmail.com>
2020-12-28 20:48:34 +0900

    add-travelnew.vue-api叩けるように調整

--- tabimae-api/app/controllers/v1/travels_controller.rb
+++ tabimae-api/app/controllers/v1/travels_controller.rb
@@ -1,2 +1,17 @@
 class V1::TravelsController < ApplicationController
+  def create
+    travel = Travel.new(travel_params)
+    if travel.save
+      render json: travel, status: :created
+    else
+      render json: travel.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+    def travel_params
+      params.require(:travel).permit(:transport, :name, :user_id)
+    end
 end
--- tabimae-web/pages/travelnew.vue
+++ tabimae-web/pages/travelnew.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>新規登録画面</h1>
     <v-container class="px-0" fluid>
-    <v-radio-group v-model="radioGroup">
+    <v-radio-group v-model="transport">
       <v-radio
         label="列車"
         value="train"
@@ -12,17 +12,31 @@
         value="air"
       ></v-radio>
     </v-radio-group>
+      <v-btn @click="handleSubmit">決定</v-btn>
   </v-container>
   </div>
 </template>
 
 
 <script>
+import axios from "@/plugins/axios";
 export default {
     data () {
       return {
-        radioGroup: [],
+        transport: "",
       }
     },
+  methods: {
+    handleSubmit() {
+      const travel = {
+        transport: this.transport,
+        user_id: this.user.id
+      };
+      this.transport = "";
+    async travelnew (travel) {
+      const res = await axios.post(travelnew, { travel });
+      }
+    }
+  }
   };
 </script>

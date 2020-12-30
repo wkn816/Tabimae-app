63bfd15db64be9d74e63359ad192287e38849e95
wkn816 <wkn816@gmail.com>
2020-12-30 16:28:47 +0900

    add-train_controller

---
+++ tabimae-api/app/controllers/v1/trains_controller.rb
@@ -0,0 +1,2 @@
+class V1::TrainsController < ApplicationController
+end
---
+++ tabimae-api/test/controllers/v1/trains_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class V1::TrainsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

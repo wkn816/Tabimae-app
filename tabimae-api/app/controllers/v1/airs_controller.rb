45c3793936bad3309eb1b6a044927c240d4a839e
wkn816 <wkn816@gmail.com>
2021-01-02 18:08:35 +0900

    uodate-air_controller追記追記

--- tabimae-api/app/controllers/v1/airs_controller.rb
+++ tabimae-api/app/controllers/v1/airs_controller.rb
@@ -1,2 +1,17 @@
 class V1::AirsController < ApplicationController
+  def create
+    air = Air.new(air_params)
+    if air.save
+      render json: air, status: :created
+    else
+      render json: air.errors, status: :unprocessable_entity
+    end
+  end
+
+
+    private
+
+    def air_params
+      params.require(:air).permit(:departure_day, :departure_time, :arrival_time, :departure_place, :arrival_place, :user_id)
+    end
 end

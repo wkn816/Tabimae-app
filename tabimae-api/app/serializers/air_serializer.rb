73de95852877ed61fe2a457b6edab312e16fa573
wkn816 <wkn816@gmail.com>
2021-01-02 23:14:50 +0900

    update-train/air_place_date

--- tabimae-api/app/serializers/air_serializer.rb
+++ tabimae-api/app/serializers/air_serializer.rb
@@ -1,8 +1,8 @@
 class AirSerializer < ActiveModel::Serializer
-  attributes :id, :departure_day, :departure_time, :arrival_time, :departure_place, :arrival_place, :flight_number, :airline, :user_id, :travel_id, :username
+  attributes :id, :departure_day, :departure_time, :arrival_time, :departure_place, :arrival_place, :flight_number, :airline, :user_id, :travel_id
   belongs_to :travel
 
-  def username
-    object.user.name
-  end
+  # def username
+  #   object.user.name
+  # end
 end
--- tabimae-web/pages/travel_new.vue
+++ tabimae-web/pages/travel_new.vue
@@ -17,9 +17,22 @@
           <v-text-field v-model="departure_place" :counter="10" label="出発地" required></v-text-field>
           <v-text-field v-model="arrival_place" :counter="10" label="到着地" required></v-text-field>
         </v-col>
+        <v-row justify="center">
+        <v-col cols="8">
+          <v-text-field v-model="departure_day" single-line>
+            <template v-slot:append-outer>
+              <date-picker v-model="departure_day"/>
+            </template>
+          </v-text-field>
+        </v-col>
+        <v-col cols="8">
+          <p>日付： {{ departure_day }}</p>
+        </v-col>
+      </v-row>
       </template>
 
       <template v-if="transport === 'air'">
+        <h1>飛行機で行く</h1>
       <v-text-field v-model="departure_place" :counter="10" label="出発地" required></v-text-field>
       <v-text-field v-model="arrival_place" :counter="10" label="到着地" required></v-text-field>
       </template>
@@ -46,26 +59,27 @@ export default {
       departure_place: "",
       arrival_place: "",
       success: false,
-      departure_date: ""
+      departure_day: null,
     };
   },
   methods: {
     async createTravel() {
-      const travel = {
+      const travel_params = {
         transport: this.transport,
         name: this.name,
         user_id: this.$store.state.auth.currentUser.id
       };
-      console.log(travel);
-      const { data } = await axios.post("/v1/travels", { travel });
+      console.log(travel_params);
+      const { data } = await axios.post("/v1/travels", { travel: travel_params });
       console.log(data.id);
       this.transport = "";
       this.name = "";
       this.success = true;
 
       if (this.transport === "air") {
+        console.log(data);
         const air_params = {
-          travel_id: data.data.id,
+          travel_id: data.id,
           departure_place: this.departure_place,
           arrival_place: this.arrival_place,
           user_id: this.$store.state.auth.currentUser.id
@@ -75,19 +89,19 @@ export default {
           //カラムたくさん追加します
         };
         console.log(air_params);
-        const res_air = await axios.post("/v1/airs", { air_params });
+        const res_air = await axios.post("/v1/airs", { air: air_params });
         console.log(res_air);
         this.departure_place = "";
         this.arrival_place = "";
       } else {
         const train_params = {
-          travel_id: data.data.id,
+          travel_id: data.id,
           departure_place: this.departure_place,
           arrival_place: this.arrival_place,
           user_id: this.$store.state.auth.currentUser.id
         };
         console.log(train_params);
-        const res_train = await axios.post("/v1/trains", { train_params });
+        const res_train = await axios.post("/v1/trains", { train: train_params });
         console.log(res_train);
         this.departure_place = "";
         this.arrival_place = "";

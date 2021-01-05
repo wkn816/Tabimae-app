f8d4b86d993e188457cc48fe291c56f83fd7862f
wkn816 <wkn816@gmail.com>
2021-01-05 23:57:20 +0900

    travel_controller叩く

--- tabimae-web/pages/travel_list.vue
+++ tabimae-web/pages/travel_list.vue
@@ -1,35 +1,61 @@
 <template>
 <div>
 
-  {{ data }}
+  <!-- {{ data }} -->
 
-  {{ transport }}
+  <!-- {{ transport }} -->
+  <!-- {{ travels }} -->
+  <!-- {{ trains }} -->
+  {{result}}
 </div>
 </template>
 
 <script>
+  // import TravelNew from "@/pages/travel_new";
+  import axios from "@/plugins/axios";
+
 // import axios from "axios"
 
 // const url = "test/api/path"
 
 export default {
+
   data() {
     return {
+      result: "",
+      travel: "",
       data: "",
       transport: ""
     }
   },
-    async asyncData() {
-      const result = await axios.get("/v1/travels", { travel });
-      return {
-        transport: this.transport
-      }
+  // computed: {
+    // user() {
+    //   return this.$store.state.auth.currentUser;
+    // },
+    // travels(){
+    //   return this.$store.state.auth.travels;
+    // },
+    // trains(){
+    //   return this.$store.state.auth.trains;
+    // }
+    // departure_day_test () {
+    //   return this.choice_departure_day ? moment(this.choice_departure_daye).format('dddd, MMMM Do YYYY') : ''
+    // },
+  // },
+    methods:{
+      async asyncData() {
+        const result = await axios.get("/v1/users", { user });
+      console.log(result);
+      // return {
+        //   transport: this.transport
+      // }
     },
-  methods: {
-    fetch() {
-      this.transport = createTravel()
     }
-  }
+  // methods: {
+  //   fetch() {
+  //     this.transport = createTravel()
+  //   }
+  // }
 }
 </script>
 

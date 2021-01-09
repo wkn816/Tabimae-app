597f9855c94b22017f562249213667f9c73fa917
wkn816 <wkn816@gmail.com>
2021-01-09 21:30:52 +0900

    fix-リンク先

--- tabimae-web/pages/travel_list.vue
+++ tabimae-web/pages/travel_list.vue
@@ -10,7 +10,7 @@
         <li>{{ train.arrival_place }}</li>
         <li>{{ train.departure_time }}</li>
         <li>{{ train.arrival_time }}</li>
-        <nuxt-link :to="`/travels/${travel.id}`">詳細</nuxt-link>
+        <nuxt-link :to="`/travel/${travel.id}`">詳細</nuxt-link>
       </ul>
       <ul v-for="train in travel.airs">
         <li>{{ air.departure_day }}</li>

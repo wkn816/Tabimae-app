81de3d6f32979e189150d63214426bb307192d2c
wkn816 <wkn816@gmail.com>
2021-01-09 11:10:08 +0900

    fix-ログイン状態によってヘッダー変える

--- tabimae-web/layouts/default.vue
+++ tabimae-web/layouts/default.vue
@@ -7,7 +7,6 @@
       fixed
       app
     >
-
       <v-list>
         <v-list-item
           v-for="(item, i) in items"
@@ -16,10 +15,9 @@
           router
           exact
         >
-        <nuxt-link to="/travel_list"></nuxt-link>
+          <nuxt-link to="/travel_list"></nuxt-link>
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
-
           </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title v-text="item.title" />
@@ -27,38 +25,21 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
+      <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>mdi-application</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-
+      <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>mdi-minus</v-icon>
       </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
       <v-btn @click="logOut">ログアウト</v-btn>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
     </v-app-bar>
@@ -67,12 +48,7 @@
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
       <v-list>
         <v-list-item @click.native="right = !right">
           <v-list-item-action>
@@ -84,63 +60,90 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
+    <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
     <template v-if="this.$store.state.auth.currentUser">
-        <nuxt />
+      <nuxt />
     </template>
-    </v-app>
+  </v-app>
 </template>
 
 <script>
 import firebase from "@/plugins/firebase";
 export default {
-  data () {
+  data() {
     return {
       clipped: false,
       drawer: false,
       fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: '新規登録',
-          to: '/travel_new'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: '旅行一覧',
-          to: '/travel_list'
-        }
-      ],
+      // items: [
+      //   {
+      //     icon: 'mdi-apps',
+      //     title: '新規登録',
+      //     to: '/travel_new'
+      //   },
+      //   {
+      //     icon: 'mdi-chart-bubble',
+      //     title: '旅行一覧',
+      //     to: '/travel_list'
+      //   }
+      // ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Tabimae'
-    }
+      title: "Tabimae"
+    };
   },
   computed: {
     user() {
       return this.$store.state.auth.currentUser;
     },
+    items() {
+      if (this.user) {
+        return [
+          {
+            icon: "mdi-apps",
+            title: "Todos",
+            to: "/"
+          },
+          {
+            icon: "mdi-chart-bubble",
+            title: "mypage",
+            to: "/mypage"
+          }
+        ];
+      } else {
+        return [
+          {
+            icon: "mdi-apps",
+            title: "ログイン",
+            to: "/login"
+          },
+          {
+            icon: "mdi-chart-bubble",
+            title: "新規登録",
+            to: "/signup"
+          }
+        ];
+      }
+    }
   },
+
   methods: {
     async logOut() {
       await firebase
         .auth()
         .signOut()
-        .catch((error) => {
+        .catch(error => {
           console.log(error);
         });
 
       this.$store.commit("setUser", null);
       this.$router.push("/login");
-    },
-  },
-}
+    }
+  }
+};
 </script>
 
 <style scoped>
@@ -149,14 +152,14 @@ export default {
   margin-top: 20px;
 }
 * {
-    margin: 0;
-  }
-  v-app {
-    background-size: 100%;
-  }
-  .bg {
-    background-color:seagreen;
-    background-size: 100%;
-    color: red;
-  }
+  margin: 0;
+}
+v-app {
+  background-size: 100%;
+}
+.bg {
+  background-color: seagreen;
+  background-size: 100%;
+  color: red;
+}
 </style>

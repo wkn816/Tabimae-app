7f96ab40cf3698ffafafbcd6257f4e2e3498a501
wkn816 <wkn816@gmail.com>
2021-01-09 20:34:18 +0900

    fix-詳細画面

---
+++ tabimae-web/pages/travel/_id.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-card>
+    <h1>詳細画面</h1>
+    {{ travelData }}
+    {{ id }}
+  </v-card>
+</template>
+
+<script>
+import axios from "@/plugins/axios";
+
+export default {
+  data() {
+    return {
+      // travel_params: "",
+      //   {
+      travelData: {},
+      travel: "",
+      transport: "",
+      userName: {},
+      id: ""
+      // name: "",
+      // departure_place: "",
+      // arrival_place: "",
+      // departure_time: "",
+      // arrival_time: "",
+    };
+  },
+  // async asyncData({ params }) {
+  //   // console.log(this.$store.state.auth.currentUser.id);
+  //   // const user = this.$store.state.auth.currentUser;
+  //   // debugger
+  //   if (user) {
+  //     //this.travelData = await axios.get("/v1/travels/${ travel.id }");
+  //     // console.log(travel.id);
+  //     this.travelData = await axios.get(
+  //       `${process.env.BASE_URL}/travels/${params.id}`
+  //     );
+  //     console.log(this.travelData);
+
+  //     //filterで作り直された配列がtravelNameに入る
+  //     console.log(this.travelData);
+  //   }
+  // },
+  async asyncData({ params }) {
+    try {
+      // debugger
+      const res = await axios.get(
+        // `${process.env.BASE_URL}/v1/travels/${params.id}`
+        // `http://localhost:8080/travels/${params.id}`
+        "/v1/travels/${ travel.id }"
+      );
+      console.log(res);
+      return {
+        res
+      };
+    } catch (err) {
+      console.log("err", err);
+    }
+  },
+
+  // created() {
+  //   this.userName = this.travelData.data.filter(function(value) {
+  //     console.log(value);
+  //   });
+  // },
+  methods: {
+    filteingUserName() {
+      // console.log(this.travelData);
+      // this.userName = this.travelData.data.map(function(value) {
+      //   console.log(value.username.user);
+      //   return value.username;
+      // });
+      // console.log(this.userName);
+    }
+  },
+  computed: {
+    // returnUserName() {
+    //   if (this.travelData !== undefined){ return }
+    //   console.log(this.travelData.data);
+    //   const userName = this.travelData.data.filter(function(value) {
+    //     console.log(value);
+    //   });
+    //   // return "アイウエオ";
+    // }
+    // user() {
+    //   return this.$store.state.auth.currentUser;
+    // }
+  }
+};
+</script>
+
+<style></style>

01a7cd7720e5ba5e7566972acfe6ea7012b4b95a
wkn816 <wkn816@gmail.com>
2021-01-10 23:08:20 +0900

    fix-出発時間情報追加

--- tabimae-web/pages/travel/_id.vue
+++ tabimae-web/pages/travel/_id.vue
@@ -7,6 +7,7 @@
     {{res_travel_show.data.trains[0].departure_day}}
     {{res_travel_show.data.trains[0].departure_place}}
     {{res_travel_show.data.trains[0].arrival_place}}
+    {{res_travel_show.data.trains[0].departure_time}}
 
   </v-card>
 </template>

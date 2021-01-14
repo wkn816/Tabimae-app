1d4b550a6b9911cd834e428f943a504967d70b37
wkn816 <wkn816@gmail.com>
2021-01-14 19:39:14 +0900

    add-ゲストログイン処理

--- tabimae-web/pages/index.vue
+++ tabimae-web/pages/index.vue
@@ -22,37 +22,24 @@ export default {
   },
   methods: {
     async guestLogin() {
-      this.$store.commit("setLoading", true);
-        const res = await firebase
-        .auth()
-        .signInWithEmailAndPassword("testtest777@gmail.com", "aaaaaa")
-        .catch((error) => {
-            this.error = ((code) => {
+        firebase
+          .auth()
+          .signInWithEmailAndPassword("testtest777@gmail.com", process.env.GUESTPW)
+          .catch(error => {
+            console.log(error);
+            this.error = (code => {
               switch (code) {
-                case "auth/email-already-in-use":
-                  return "既にそのメールアドレスは使われています";
+                case "auth/user-not-found":
+                  return "メールアドレスが間違っています";
                 case "auth/wrong-password":
                   return "※パスワードが正しくありません";
-                case "auth/weak-password":
-                  return "※パスワードは最低6文字以上にしてください";
                 default:
                   return "※メールアドレスとパスワードをご確認ください";
               }
             })(error.code);
           });
-          const user = {
-          email: res.user.email,
-          name: this.name,
-          uid: res.user.uid,
-        };
-        await axios.post("/v1/users", {
-          user
-        }).catch((err) => {
-          console.log({
-            err
-          });
-        });
-        this.$router.push("/travel_list");
+            this.$router.push("/travel_list");
+      },
         // .catch(error => {
         //   console.log(error);
         //   this.error = (code => {
@@ -67,7 +54,7 @@ export default {
         //   })(error.code);
         // });
     },
-  },
+  // },
 };
 </script>
 

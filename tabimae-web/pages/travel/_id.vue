6f76e32714bf45f3183f99a7492872aae8f6cdc4
wkn816 <wkn816@gmail.com>
2021-01-14 19:55:44 +0900

    add-モダールMSG追加

--- tabimae-web/pages/travel/_id.vue
+++ tabimae-web/pages/travel/_id.vue
@@ -11,137 +11,145 @@
     {{ res_travel_show.data.trains[0].arrival_time }}
     <v-icon small @click="deleteItem({ res_travel_show })">削除</v-icon>
 
-<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-<modal name="hello-world">
-  {hello, world!}
-</modal>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <div>
+      <button @click="show">クリックする</button>
+
+      <modal name="modal-content">
+        <p style="color:black">モーダルウィンドウで表示されるコンテンツ</p>
+        <button @click="hide">
+          <p style="color:black">閉じる</p>
+        </button>
+
+      </modal>
+    </div>
   </v-card>
 </template>
 
 
 <script>
-import axios from "@/plugins/axios";
-import moment from "moment";
-import Vue from 'vue'
-import VModal from 'vue-js-modal'
+  import axios from "@/plugins/axios";
+  import moment from "moment";
+  import Vue from 'vue'
+  import VModal from 'vue-js-modal'
 
-Vue.use(VModal)
+  Vue.use(VModal)
 
-export default {
-  // props: ["travel"],
-  data() {
-    return {
-      res_travel_show: {},
-      res_delete: {},
-      test: {}
-
-    };
-  },
-  // console.log(array1[0]);
-
-  // async asyncData({ params }) {
-  //   // console.log(this.$store.state.auth.currentUser.id);
-  //   // const user = this.$store.state.auth.currentUser;
-  //   // debugger
-  //   if (user) {
-  //     //this.travelData = await axios.get("/v1/travels/${ travel.id }");
-  //     // console.log(travel.id);
-  //     this.travelData = await axios.get(
-  //       `${process.env.BASE_URL}/travels/${params.id}`
-  //     );
-  //     console.log(this.travelData);
-
-  //     //filterで作り直された配列がtravelNameに入る
-  //     console.log(this.travelData);
-  //   }
-  // },
-  async asyncData({ params }) {
-    try {
-      // debugger
-      const res_travel_show = await axios.get(
-        // `${process.env.BASE_URL}/v1/travels/${params.id}`
-        // `http://localhost:8080/travels/${params.id}`
-        `/v1/travels/${params.id}`
-      );
-      const departure_day = moment(
-        res_travel_show.data.trains[0].departure_day
-      );
-      // // debugger
-      const daylimit = departure_day.diff(moment(), "days"); // 91
-      let text;
-      if (daylimit > 10) {
-        text = "ゆっくりでOK";
-      } else if (daylimit > 2 && daylimit < 10) {
-        text = "そろそろ";
-      } else if (daylimit == 1) {
-        text = "寝坊しないように";
-      } else if (daylimit == 0) {
-        text = "当日";
-      } else {
-        text = "おわた";
-      }
-      // console.log(res_travel_show);
+  export default {
+    // props: ["travel"],
+    data() {
       return {
-        res_travel_show,
-        daylimit,
-        text
+        res_travel_show: {},
+        res_delete: {},
+        test: {}
+
       };
-    } catch (err) {
-      console.log("err", err);
-    }
-    // const daylimit =
-  },
+    },
+    // console.log(array1[0]);
 
-  methods: {
-    async deleteItem(res_travel_show) {
-      // debugger
-      console.log(res_travel_show);
-      const res = confirm("本当に削除しますか？");
-      let deleteres;
-      if (res) {
-        deleteres = await axios.delete(
-          `/v1/travels/${res_travel_show.res_travel_show.data.id}`
+    // async asyncData({ params }) {
+    //   // console.log(this.$store.state.auth.currentUser.id);
+    //   // const user = this.$store.state.auth.currentUser;
+    //   // debugger
+    //   if (user) {
+    //     //this.travelData = await axios.get("/v1/travels/${ travel.id }");
+    //     // console.log(travel.id);
+    //     this.travelData = await axios.get(
+    //       `${process.env.BASE_URL}/travels/${params.id}`
+    //     );
+    //     console.log(this.travelData);
+
+    //     //filterで作り直された配列がtravelNameに入る
+    //     console.log(this.travelData);
+    //   }
+    // },
+    async asyncData({
+      params
+    }) {
+      try {
+        // debugger
+        const res_travel_show = await axios.get(
+          // `${process.env.BASE_URL}/v1/travels/${params.id}`
+          // `http://localhost:8080/travels/${params.id}`
+          `/v1/travels/${params.id}`
         );
+        const departure_day = moment(
+          res_travel_show.data.trains[0].departure_day
+        );
+        // // debugger
+        const daylimit = departure_day.diff(moment(), "days"); // 91
+        let text;
+        if (daylimit > 10) {
+          text = "ゆっくりでOK";
+        } else if (daylimit > 2 && daylimit < 10) {
+          text = "そろそろ";
+        } else if (daylimit == 1) {
+          text = "寝坊しないように";
+        } else if (daylimit == 0) {
+          text = "当日";
+        } else {
+          text = "おわた";
+        }
+        // console.log(res_travel_show);
+        return {
+          res_travel_show,
+          daylimit,
+          text
+        };
+      } catch (err) {
+        console.log("err", err);
       }
-      // debugger
-      if (deleteres.status == 200) {
-        this.$router.push("/travel_list");
-      }
+      // const daylimit =
     },
 
-    show() {
-      this.$modal.show("modal-content");
-      {test =aaa}
-    },
-    hide() {
-      this.$modal.hide("modal-content");
+    methods: {
+      async deleteItem(res_travel_show) {
+        // debugger
+        console.log(res_travel_show);
+        const res = confirm("本当に削除しますか？");
+        let deleteres;
+        if (res) {
+          deleteres = await axios.delete(
+            `/v1/travels/${res_travel_show.res_travel_show.data.id}`
+          );
+        }
+        // debugger
+        if (deleteres.status == 200) {
+          this.$router.push("/travel_list");
+        }
+      },
+
+      show() {
+        this.$modal.show("modal-content");
+      },
+      hide() {
+        this.$modal.hide("modal-content");
+      }
+
+      // }
+      // methods: {
+      // async deleteItem(id) {
+      //   console.log(IDBCursor);
+      //   const res_delete = await this.$axios.$delete(`/v1/travels/${params.id}`);
+      // }
+      // }
+      // computed: {
+      // returnUserName() {
+      //   if (this.travelData !== undefined){ return }
+      //   console.log(this.travelData.data);
+      //   const userName = this.travelData.data.filter(function(value) {
+      //     console.log(value);
+      //   });
+      //   // return "アイウエオ";
+      // }
+      // user() {
+      //   return this.$store.state.auth.currentUser;
+      // }
     }
+  };
 
-    // }
-    // methods: {
-    // async deleteItem(id) {
-    //   console.log(IDBCursor);
-    //   const res_delete = await this.$axios.$delete(`/v1/travels/${params.id}`);
-    // }
-    // }
-    // computed: {
-    // returnUserName() {
-    //   if (this.travelData !== undefined){ return }
-    //   console.log(this.travelData.data);
-    //   const userName = this.travelData.data.filter(function(value) {
-    //     console.log(value);
-    //   });
-    //   // return "アイウエオ";
-    // }
-    // user() {
-    //   return this.$store.state.auth.currentUser;
-    // }
-  }
-};
 </script>
 
 <style>
 
 </style>
-
-

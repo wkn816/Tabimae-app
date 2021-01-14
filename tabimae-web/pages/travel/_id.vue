ad902c7e79922390db174ddae86caa7460752903
wkn816 <wkn816@gmail.com>
2021-01-14 21:50:05 +0900

    fix-モーダル画面題名変更

--- tabimae-web/pages/travel/_id.vue
+++ tabimae-web/pages/travel/_id.vue
@@ -16,7 +16,7 @@
       <button @click="show">忘れ物リスト</button>
 
       <modal name="modal-content">
-        <p style="color:black">モーダルウィンドウで表示されるコンテンツ</p>
+        <h2 style="color:black">忘れ物リスト</h2>
         <button @click="hide">
           <p style="color:black">閉じる</p>
         </button>

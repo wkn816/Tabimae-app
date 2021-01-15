851201d5a19c56a487813bd8dfc5129d4ad810da
wkn816 <wkn816@gmail.com>
2021-01-16 03:37:30 +0900

    add-モーダルコンポーネント

---
+++ tabimae-web/components/Modal.vue
@@ -0,0 +1,60 @@
+<template>
+  <transition name="modal" appear>
+    <div class="modal__overlay">
+      <div class="modal__window">
+        <div class="modal__content">
+          <slot />
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<style lang="scss" scoped>
+.modal {
+  &__overlay {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: fixed;
+    z-index: 100;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background: rgba(0, 0, 0, 0.7);
+  }
+
+  &__window {
+    height: 70%;
+    width: 70%;
+    overflow: hidden;
+    background-color: aquamarine;
+  }
+
+  &__content {
+    height: 100%;
+    padding: 30px;
+  }
+}
+
+// transition
+.modal-enter-active,
+.modal-leave-active {
+  transition: opacity 0.4s;
+  .modal__window {
+    transition: opacity 0.4s, transform 0.4s;
+  }
+}
+.modal-leave-active {
+  transition: opacity 0.6s ease 0.4s;
+}
+.modal-enter,
+.modal-leave-to {
+  opacity: 0;
+  .modal__window {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+}
+</style>

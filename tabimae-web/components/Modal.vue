73ddb81ee79f476df2cca7adf537d15236f5d977
wkn816 <wkn816@gmail.com>
2021-01-16 04:25:57 +0900

    fix-modal画面背景色変更

--- tabimae-web/components/Modal.vue
+++ tabimae-web/components/Modal.vue
@@ -11,51 +11,56 @@
 </template>
 
 <style lang="scss" scoped>
-.modal {
-  &__overlay {
-    display: flex;
-    align-items: center;
-    text-align: center;
-    justify-content: center;
-    position: fixed;
-    z-index: 100;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-    background: rgba(0, 0, 0, 0.7);
+  .modal {
+    &__overlay {
+      display: flex;
+      align-items: center;
+      text-align: center;
+      justify-content: center;
+      position: fixed;
+      z-index: 100;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 100%;
+      background: rgba(0, 0, 0, 0.7);
+    }
+
+    &__window {
+      height: 70%;
+      width: 70%;
+      overflow: hidden;
+      background-color: #C0C0C0;
+    }
+
+    &__content {
+      height: 100%;
+      // padding: 200px;
+    }
   }
 
-  &__window {
-    height: 70%;
-    width: 70%;
-    overflow: hidden;
-    background-color: aquamarine;
+  // transition
+  .modal-enter-active,
+  .modal-leave-active {
+    transition: opacity 0.4s;
+
+    .modal__window {
+      transition: opacity 0.4s, transform 0.4s;
+    }
   }
 
-  &__content {
-    height: 100%;
-    padding: 200px;
+  .modal-leave-active {
+    transition: opacity 0.6s ease 0.4s;
   }
-}
 
-// transition
-.modal-enter-active,
-.modal-leave-active {
-  transition: opacity 0.4s;
-  .modal__window {
-    transition: opacity 0.4s, transform 0.4s;
-  }
-}
-.modal-leave-active {
-  transition: opacity 0.6s ease 0.4s;
-}
-.modal-enter,
-.modal-leave-to {
-  opacity: 0;
-  .modal__window {
+  .modal-enter,
+  .modal-leave-to {
     opacity: 0;
-    transform: translateY(-20px);
+
+    .modal__window {
+      opacity: 0;
+      transform: translateY(-20px);
+    }
   }
-}
+
 </style>

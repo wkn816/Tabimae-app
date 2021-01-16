4b988b65fa54f86055c861a63e667c3e7a664edf
wkn816 <wkn816@gmail.com>
2021-01-16 21:28:06 +0900

    fix-設定変更

--- tabimae-web/jest.config.js
+++ tabimae-web/jest.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  verbose: true
-};
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js'
+  },
+  moduleFileExtensions: [
+    'js',
+    'vue',
+    'json'
+  ],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/**/*.vue'
+  ],
+  setupFiles: ['./spec/setup.js']
+}

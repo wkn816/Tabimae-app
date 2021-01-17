e81c28bc0a8e8e32e2e354d10bceeacca085a2b2
wkn816 <wkn816@gmail.com>
2021-01-18 03:14:16 +0900

    fix-indexに記述したtyperを移籍

--- tabimae-web/components/HomeHero.vue
+++ tabimae-web/components/HomeHero.vue
@@ -3,20 +3,30 @@
     <v-container fluid fill-height class="home-hero__content">
       <v-row>
         <v-col class="home-hero__content-text">
-          <p>Welcome to my website.</p>
-          <p>I'm a enginner.</p>
+          <vue-typer text="タビマエ"></vue-typer>
         </v-col>
       </v-row>
     </v-container>
   </section>
 </template>
+
+<script>
+import { VueTyper } from 'vue-typer'
+
+
+export default {
+components: {
+    VueTyper
+  },
+}
+</script>
 <style  lang="scss" scoped>
 .home-hero__content {
   // background: url("../assets/img/ppf.jpg");
-  // background-size: cover;
-  // background-position: center center;
-  // width: 100%;
-  // height: 50vh;
+  background-size: cover;
+  background-position: center center;
+  width: 100%;
+  height: 100vh;
 
   &-text {
     color: white;
@@ -25,11 +35,5 @@
     font-weight: bold;
   }
 }
-.home-hero{
-  background: url("../assets/img/ppf.jpg");
-  background-size: cover;
-  background-position: center center;
-  width: 100%;
-  height: 50vh;
-}
+
 </style>

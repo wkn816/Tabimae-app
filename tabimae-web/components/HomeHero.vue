64bd616d483b108813a6aa091c196672e938f651
wkn816 <wkn816@gmail.com>
2021-01-18 03:40:03 +0900

    add-typerのテキスト追加しました

--- tabimae-web/components/HomeHero.vue
+++ tabimae-web/components/HomeHero.vue
@@ -3,8 +3,15 @@
     <v-container fluid fill-height class="home-hero__content">
       <v-row>
         <v-col class="home-hero__content-text">
-          <vue-typer text="タビマエ"></vue-typer>
-        </v-col>
+          <vue-typer text="タビマエ" :repeat="1"></vue-typer></br>
+
+<vue-typer style="font-family: dot; text-align:center;" :text="[
+        '＊出発前に抜かりのない準備と',
+        '＊お役立ち情報をGETして',
+        '＊いつもよりワンランク上の',
+        '旅行をたのしみましょう！',
+        ]" erase-style="clear" :type-delay="150" :pre-type-delay="90"	:erase-delay="300" :repeat="Infinity"></vue-typer>
+      </v-col>
       </v-row>
     </v-container>
   </section>

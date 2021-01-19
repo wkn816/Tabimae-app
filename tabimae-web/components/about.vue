274b7bf992c078aa8d8ca27bc691a7ebee8917cb
wkn816 <wkn816@gmail.com>
2021-01-19 22:14:58 +0900

    fix-アプリ紹介文完成

--- tabimae-web/components/about.vue
+++ tabimae-web/components/about.vue
@@ -2,26 +2,27 @@
 <div>
     <v-col sm="11" md="11" lg="12"
           offset-lg="1">
-<v-card>
-  <v-card-title>
+<v-card color=#f3d2c1>
+  <v-card-title primary-title class="justify-center">
     <v-icon
         large
+        color=#001858
       >
         mdi-bag-checked
       </v-icon>
       <span class="title">タビマエ</span>
   </v-card-title>
-<v-card-text class="text1">
-      旅行のスケジュール管理、持ち物チェック管理ができます。<br>
-      出発日までの残りの日数を計算し「いつ」「何を」準備すれば良いかをお知らせします。
+<v-card-text color=#001858>
+      <p>旅行のスケジュール管理、持ち物チェック管理ができます。<br>
+      出発日までの残りの日数を計算し「いつ」「何を」準備すれば良いかをお知らせします。</p>
     </v-card-text>
 <v-card-text class="text2">
-  思い込みにより時間を間違えて旅行の計画が台無しに・・・。「あれ何時に出発だっけ？」正確な時間は忘れてしまいがち。<br>
-  目的地に着いてから「あれ忘れた！」と焦った経験はありませんか。旅行の準備段階で何を持って行けば良いのか分からなくなる事は多いもの。
+  <p>思い込みにより時間を間違えて旅行の計画が台無しに・・・。「あれ何時に出発だっけ？」正確な時間は忘れてしまいがち。<br>
+  目的地に着いてから「あれ忘れた！」と焦った経験はありませんか。旅行の準備段階で何を持って行けば良いのか分からなくなる事は多いもの。</p>
 </v-card-text>
 <v-card-text class="text３">
-  旅行は行く前から始まっています。<br>
-  旅行後（タビアト）に後悔のない素敵な旅行を”タビマエ”と創造しましょう！
+  <p>旅行は行く前から始まっています。<br>
+  旅行後（タビアト）に後悔のない素敵な旅行を”タビマエ”と創造しましょう！</p>
   </v-card-text>
 </v-card>
     </v-col>
@@ -39,10 +40,22 @@ export default {
 <style lang="scss" scoped>
 .v-card{
   width: 100%;
+  text-align: center;
 }
 .v-card-title{
   text-align: center;
   font-weight: bolder;
 }
+.title{
+  color: #001858;
+  text-align: center;
+}
+p{
+  color: #001858;
+  text-align: center;
+
+}
+
+
 
 </style>

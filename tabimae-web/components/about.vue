ff1e13f37ce0b803588fc09c09eb57c01f3c011e
wkn816 <wkn816@gmail.com>
2021-01-19 21:45:36 +0900

    fix-アプリの紹介文

---
+++ tabimae-web/components/about.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+    <v-col sm="11" md="11" lg="12"
+          offset-lg="1">
+<v-card>
+  <v-card-title>
+    <v-icon
+        large
+      >
+        mdi-bag-checked
+      </v-icon>
+      <span class="title">タビマエ</span>
+  </v-card-title>
+<v-card-text class="text1">
+      旅行のスケジュール管理、持ち物チェック管理ができます。<br>
+      出発日までの残りの日数を計算し「いつ」「何を」準備すれば良いかをお知らせします。
+    </v-card-text>
+<v-card-text class="text2">
+  思い込みにより時間を間違えて旅行の計画が台無しに・・・。「あれ何時に出発だっけ？」正確な時間は忘れてしまいがち。<br>
+  目的地に着いてから「あれ忘れた！」と焦った経験はありませんか。旅行の準備段階で何を持って行けば良いのか分からなくなる事は多いもの。
+</v-card-text>
+<v-card-text class="text３">
+  旅行は行く前から始まっています。<br>
+  旅行後（タビアト）に後悔のない素敵な旅行を”タビマエ”と創造しましょう！
+  </v-card-text>
+</v-card>
+    </v-col>
+
+</div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.v-card{
+  width: 100%;
+}
+.v-card-title{
+  text-align: center;
+  font-weight: bolder;
+}
+
+</style>

eaf60acb65bdb25fd125ff8cee1174ced2041fa1
wkn816 <wkn816@gmail.com>
2021-01-20 11:35:41 +0900

    fix-モダール画面の背景色変更

--- tabimae-web/components/Modal.vue
+++ tabimae-web/components/Modal.vue
@@ -11,54 +11,6 @@
 </template>
 
 <style lang="scss" scoped>
-
-.modal {
-  &__overlay {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: fixed;
-    z-index: 100;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-    background: rgba(0, 0, 0, 0.7);
-  }
-
-  &__window {
-    height: 70%;
-    width: 70%;
-    overflow: hidden;
-    background-color: aquamarine;
-  }
-
-  &__content {
-    height: 100%;
-    padding: 30px;
-  }
-}
-
-// transition
-.modal-enter-active,
-.modal-leave-active {
-  transition: opacity 0.4s;
-  .modal__window {
-    transition: opacity 0.4s, transform 0.4s;
-  }
-}
-.modal-leave-active {
-  transition: opacity 0.6s ease 0.4s;
-}
-.modal-enter,
-.modal-leave-to {
-  opacity: 0;
-  .modal__window {
-    opacity: 0;
-    transform: translateY(-20px);
-  }
-}
-</style>
   .modal {
     &__overlay {
       display: flex;
@@ -112,4 +64,3 @@
   }
 
 </style>
-

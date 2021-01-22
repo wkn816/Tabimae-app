82fd5af528593d2e6ec3a8d8881a172b266ea5f6
wkn816 <wkn816@gmail.com>
2021-01-22 17:00:17 +0900

    fix-旅行一覧画面のカード分け

--- tabimae-web/pages/travel_list.vue
+++ tabimae-web/pages/travel_list.vue
@@ -1,88 +1,143 @@
 <template>
-    <v-card>
-    <h1>一覧</h1>
-    <ul v-for="travel in travelData.data">
-      <li>{{ travel.transport }}</li>
-      <li>{{ travel.name }}</li>
-      <ul v-for="train in travel.trains">
-        <li>{{ train.departure_day }}</li>
-        <li>{{ train.departure_place }}</li>
-        <li>{{ train.arrival_place }}</li>
-        <li>{{ train.departure_time }}</li>
-        <li>{{ train.arrival_time }}</li>
-        <nuxt-link :to="`/travel/${travel.id}`">詳細へ遷移</nuxt-link>
-      </ul>
-      <ul v-for="train in travel.airs">
-        <li>{{ air.departure_day }}</li>
-      </ul>
-    </ul>
-  </v-card>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="11" md="11" lg="12">
+          <v-card color=#f3d2c1>
+            <v-card-title primary-title class="justify-center">
+              <v-icon large color=#001858>
+                mdi-bag-checked
+              </v-icon>
+              <span class="title">旅行一覧</span>
+            </v-card-title>
+          </v-card>
+          <v-card color=#f3d2c1>
+            <v-ul v-for="travel in travelData.data">
+              <!-- <li>{{ travel.transport }}</li> -->
+              <v-li>{{ travel.name }}</v-li>
+              <v-ul v-for="train in travel.trains">
+                <v-col cols="12" sm="11" md="11" lg="12">
+                  <v-li>
+                    <v-card-text class="item-text">
+                      {{ train.departure_day }}
+                    </v-card-text>
+                  </v-li>
+                </v-col>
+                <v-col cols="12" sm="11" md="11" lg="12">
+                  <v-li>{{ train.departure_place }}</v-li>
+                </v-col>
+                <v-col cols="12" sm="11" md="11" lg="12">
+                  <v-li>{{ train.arrival_place }}</v-li>
+                </v-col>
+                <v-col cols="12" sm="11" md="11" lg="12">
+                  <lv-i>{{ train.departure_time }}</lv-i>
+                </v-col>
+                <v-col cols="12" sm="11" md="11" lg="12">
+                  <v-li>{{ train.arrival_time }}</v-li>
+                </v-col>
+                <nuxt-link :to="`/travel/${travel.id}`">詳細へ遷移</nuxt-link>
+              </v-ul>
+              <!-- <ul v-for="train in travel.airs">
+                <li>{{ air.departure_day }}</li>
+              </ul> -->
+            </v-ul>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
-import axios from "@/plugins/axios";
-import TravelNew from "@/pages/travel_new";
+  import axios from "@/plugins/axios";
+  import TravelNew from "@/pages/travel_new";
 
-export default {
-  // components: {
-  //   TravelNew,
-  // },
-  data() {
-    return {
-      // travel_params: "",
-      //   {
-      travelData: {},
-      travel: "",
-      transport: "",
-      userName: {}
-      // name: "",
-      // departure_place: "",
-      // arrival_place: "",
-      // departure_time: "",
-      // arrival_time: "",
-    };
-  },
-  async fetch() {
-    // console.log(this.$store.state.auth.currentUser.id);
-    const user = this.$store.state.auth.currentUser;
-    // debugger
-    if (user) {
-      this.travelData = await axios.get("/v1/travels", { params: { user } });
+  export default {
+    // components: {
+    //   TravelNew,
+    // },
+    data() {
+      return {
+        // travel_params: "",
+        //   {
+        travelData: {},
+        travel: "",
+        transport: "",
+        userName: {}
+        // name: "",
+        // departure_place: "",
+        // arrival_place: "",
+        // departure_time: "",
+        // arrival_time: "",
+      };
+    },
+    async fetch() {
+      // console.log(this.$store.state.auth.currentUser.id);
+      const user = this.$store.state.auth.currentUser;
+      // debugger
+      if (user) {
+        this.travelData = await axios.get("/v1/travels", {
+          params: {
+            user
+          }
+        });
 
-      //filterで作り直された配列がtravelNameに入る
-      console.log(this.travelData);
-    }
-  },
+        //filterで作り直された配列がtravelNameに入る
+        console.log(this.travelData);
+      }
+    },
 
-  // created() {
-  //   this.userName = this.travelData.data.filter(function(value) {
-  //     console.log(value);
-  //   });
-  // },
-  methods: {
-    filteingUserName() {
-      // console.log(this.travelData);
-      // this.userName = this.travelData.data.map(function(value) {
-      //   console.log(value.username.user);
-      //   return value.username;
-      // });
-      // console.log(this.userName);
-    }
-  },
-  computed: {
-    // returnUserName() {
-    //   if (this.travelData !== undefined){ return }
-    //   console.log(this.travelData.data);
-    //   const userName = this.travelData.data.filter(function(value) {
+    // created() {
+    //   this.userName = this.travelData.data.filter(function(value) {
     //     console.log(value);
     //   });
-    //   // return "アイウエオ";
-    // }
-    // user() {
-    //   return this.$store.state.auth.currentUser;
-    // }
-  }
-};
+    // },
+    methods: {
+      filteingUserName() {
+        // console.log(this.travelData);
+        // this.userName = this.travelData.data.map(function(value) {
+        //   console.log(value.username.user);
+        //   return value.username;
+        // });
+        // console.log(this.userName);
+      }
+    },
+    computed: {
+      // returnUserName() {
+      //   if (this.travelData !== undefined){ return }
+      //   console.log(this.travelData.data);
+      //   const userName = this.travelData.data.filter(function(value) {
+      //     console.log(value);
+      //   });
+      //   // return "アイウエオ";
+      // }
+      // user() {
+      //   return this.$store.state.auth.currentUser;
+      // }
+    }
+  };
+
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+  .item-text {
+    color: red;
+  }
+
+  .v-card {
+    width: 100%;
+    // text-align: center;
+    margin-bottom: 40px;
+  }
+
+  .v-card-title {
+    text-align: center;
+    font-weight: bolder;
+  }
+
+  .title {
+    color: #001858;
+    text-align: center;
+  }
+
+</style>

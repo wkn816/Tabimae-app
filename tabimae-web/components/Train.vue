3209c37dcbe5c9556c1988d9b10738d70a37326d
wkn816 <wkn816@gmail.com>
2021-01-24 20:12:42 +0900

    fix-列車案内のモーダル画面の文章編集

--- tabimae-web/components/Train.vue
+++ tabimae-web/components/Train.vue
@@ -6,12 +6,23 @@
         </v-col>
 
       <Modal v-if="modalFlag">
-        <ul>
+        <!-- <ul>
           <li v-for="item in items" :key="item">
+
             <v-checkbox v-model="selected" :label="item" :value="item"></v-checkbox>
           </li>
+        </ul> -->
+        <h2 class="train-info-title">列車ご乗車の際のご注意事項</h2>
+        <ul class="train-info-text">
+          <li>新幹線ご利用の場合</li>
+          <li>新幹線は「乗車券」と「特急券」の2種類が必要です。</li>
+          <li>購入した切符の券面に記載の乗車日/乗車時間/座席を確認しましょう</li>
+          <li>東海道・山陽・九州新幹線ご利用の場合で、「3辺の合計が160cm超250cm以内」の荷物（「特大荷物」）を車内に持ち込む場合、「特大荷物スペースつき座席」をご予約ください</li>
+          <li>「特大荷物スペースつき座席」の予約なしに特大荷物を持ち込んだ場合、持込手数料¥1.000が発生します</li>
+          <li>購入した列車に乗り遅れてしまった場合、払い戻しは不可で、同日の後続列車の自由席にご乗車いただけます。<｀/li>
+          <li>新幹線ご利用の場合</li>
         </ul>
-        <button @click="closeModal">閉じる</button>
+        <button @click="closeModal" class="modal-close">閉じる</button>
       </Modal>
   </div>
 </template>
@@ -63,15 +74,27 @@
       transition-duration: 40ms;
     }
   }
-  // .main-introduction {
-  //     margin: 2em 0;
-  //     position: relative;
-  //     padding: 0.5em 1.5em;
-  //     border-top: solid 2px white;
-  //     border-bottom: solid 2px white;
-  //     font-size: 20px;
-  //     font-family: "dot";
-  //     letter-spacing: 10px;
-  // }
+.modal-close{
+    background-color: #001858;
+    font-weight: bolder;
+    // border: solid 5px #f582ae;
+    /*線*/
+    border-radius: 10px;
+    /*角の丸み*/
+    text-decoration: none;
+    display: flex;
+    -webkit-justify-content: center;
+    justify-content: center;
+    -webkit-align-items: center;
+    align-items: center;
+    box-shadow: 3px 3px 6px rgba(0, 0, 0, 0.883);
+    padding: 7px;
+}
+.train-info-title{
+  color: #001858;
+}
+.train-info-text{
+  color: #001858;
+}
 
 </style>

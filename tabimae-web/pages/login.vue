b1617f46ba9d8fb3d5e9519481fb39ce605b9404
wkn816 <wkn816@gmail.com>
2021-02-03 18:50:46 +0900

    fixーインデント

--- tabimae-web/pages/login.vue
+++ tabimae-web/pages/login.vue
@@ -1,35 +1,35 @@
 <template>
-<v-app class="bg">
-  <v-row>
+  <v-app class="bg">
+    <v-row>
       <v-col cols="12" sm="10" md="6" lg="6" offset-lg="3">
-      <v-card color=#f3d2c1 class="ttt">
-        <v-card-title primary-title class="justify-center">
-          <v-icon large color=#001858>
-            mdi-bag-checked
-          </v-icon>
-          <span class="title">ログイン</span>
+        <v-card color=#f3d2c1 class="ttt">
+          <v-card-title primary-title class="justify-center">
+            <v-icon large color=#001858>
+              mdi-bag-checked
+            </v-icon>
+            <span class="title">ログイン</span>
 
-        </v-card-title>
-        <v-card-text color=#001858>
-        <form>
-          <v-text-field v-model="email" :counter="20" label="email" data-vv-name="email" required input-class="ttt">
-          </v-text-field>
-          <v-text-field v-model="password" label="password" data-vv-name="password" required
-            :type="show1 ? 'text' : 'password'" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-            @click:append="show1 = !show1"></v-text-field>
-<v-hover v-slot:default="{ hover }">
-          <v-btn block dark class="mr-4" @click="login" color="#001858" elevation="5">
-                          <v-icon v-text="hover ? 'mdi-account-search' : ''">mdi-account-search-outline</v-icon>ログイン
+          </v-card-title>
+          <v-card-text color=#001858>
+            <form>
+              <v-text-field v-model="email" :counter="20" label="email" data-vv-name="email" required input-class="ttt">
+              </v-text-field>
+              <v-text-field v-model="password" label="password" data-vv-name="password" required
+                :type="show1 ? 'text' : 'password'" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="show1 = !show1"></v-text-field>
+              <v-hover v-slot:default="{ hover }">
+                <v-btn block dark class="mr-4" @click="login" color="#001858" elevation="5">
+                  <v-icon v-text="hover ? 'mdi-account-search' : ''">mdi-account-search-outline</v-icon>ログイン
 
-          </v-btn>
-</v-hover>
-          <p v-if="error" class="errors">{{ error }}</p>
-        </form>
-        </v-card-text>
-      </v-card>
-    </v-col>
-  </v-row>
-  <v-row>
+                </v-btn>
+              </v-hover>
+              <p v-if="error" class="errors">{{ error }}</p>
+            </form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
       <v-col cols="12" sm="10" md="6" lg="6" offset-lg="3">
 
         <v-card color=#f3d2c1>
@@ -50,7 +50,7 @@
       </v-col>
     </v-row>
 
-</v-app>
+  </v-app>
 
 </template>
 
@@ -103,20 +103,23 @@
     font-weight: bolder;
   }
 
-  .title{
-  color: #001858;
-  text-align: center;
-}
-.mr-4{
-  cursor: pointer;
+  .title {
+    color: #001858;
+    text-align: center;
+  }
+
+  .mr-4 {
+    cursor: pointer;
     display: block;
     text-align: center;
     height: 150px;
     border-radius: 8px;
     border: 1px solid #dddddd;
     box-shadow: 4px 4px #f582ae;
-}
-.ttt{
-  margin-top: 200px;
-}
+  }
+
+  .ttt {
+    margin-top: 200px;
+  }
+
 </style>

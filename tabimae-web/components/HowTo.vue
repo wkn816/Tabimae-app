bfba1be2d3140c71caca193f113c63265f35503d
wkn816 <wkn816@gmail.com>
2021-02-15 18:20:22 +0900

    update-案内画像をコンポーネント化しました

--- tabimae-web/components/HowTo.vue
+++ tabimae-web/components/HowTo.vue
@@ -76,6 +76,15 @@
 
 </script>
 <style lang="scss" scoped>
+
+$sp: 480px;  // スマホ
+
+@mixin sp {
+  @media (max-width: ($sp)) {
+    @content;
+  }
+}
+
   h1 {
     color: black;
   }
@@ -88,6 +97,9 @@
       transform: scale(1.04);
       transition-duration: 40ms;
     }
+    @include sp {
+          width: 250px;
+          }
   }
 
 .modal-close{

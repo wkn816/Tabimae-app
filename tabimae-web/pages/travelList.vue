430cf2be0c622c50b43b6b9b411909736b8bbdcd
wkn816 <wkn816@gmail.com>
2021-02-15 18:20:48 +0900

    update-案内画像をコンポーネント化しました

--- tabimae-web/pages/travelList.vue
+++ tabimae-web/pages/travelList.vue
@@ -58,11 +58,7 @@
                   <td class="blue lighten-5">{{ train.arrival_place }}</td>
                   <td class="blue lighten-5">{{ train.departure_time }}</td>
                   <td class="blue lighten-5">{{ train.arrival_time }}</td>
-
                 </tr>
-                <!-- </v-card> -->
-                <!-- </div> -->
-
               </tbody>
             </v-simple-table>
           </v-card>
@@ -71,33 +67,7 @@
     </v-container>
 
     <v-container>
-      <v-card color="#f3d2c1">
-        <v-card-title primary-title class="justify-center">
-          <v-icon x-large color=#e53170>
-            mdi-chevron-double-right
-          </v-icon>
-          <v-title class="display">旅行出発前に知っておきたいこと</v-title>
-          <v-icon x-large color=#e53170>
-            mdi-chevron-double-left </v-icon>
-        </v-card-title>
-        <v-card-text color="#001858">
-          <p>画像をクリックすると知っておきたい情報をチェックできます</p>
-        </v-card-text>
-        <v-row>
-            <v-col class="info-img" cols="12" offset-sm="3" sm="3" offset-md="3" md="3" offset-lg="2" lg="2">
-              <Train />
-            </v-col>
-            <v-col class="info-img" cols="12" offset-sm="6" sm="4" offset-md="3" md="3" offset-lg="2" lg="2">
-              <Air />
-            </v-col>
-            <v-col class="info-img" cols="12" offset-sm="6" sm="4" offset-md="3" md="3" offset-lg="2" lg="3">
-              <TravelEtiquette />
-            </v-col>
-            <v-col class="info-img" cols="12" offset-sm="6" sm="4" offset-md="3" md="3" offset-lg="1" lg="1">
-              <HowTo />
-            </v-col>
-          </v-row>
-      </v-card>
+      <TravelInfo />
     </v-container>
 
   </div>
@@ -109,6 +79,7 @@
   import Train from "~/components/Train.vue";
   import Air from "~/components/Air.vue";
   import TravelEtiquette from "~/components/TravelEtiquette.vue";
+  import TravelInfo from "~/components/TravelInfo.vue";
 
   export default {
     // components: {
@@ -173,6 +144,7 @@
 </script>
 
 <style lang="scss" scoped>
+
   .v-card {
     width: 100%;
     // text-align: center;
@@ -209,22 +181,9 @@
     font-size: 25px;
   }
 
-  p {
-    color: #001858;
-    text-align: center;
-  }
-
   .travellist-card {
     margin-top: 80px;
   }
-
-  .display {
-    color: #001858;
-    text-align: center;
-    font-size: 30px;
-    font-family: 'TabimaeFont';
-  }
-
   .v-application a {
     color: #fffffe;
     font-weight: 100;
@@ -236,9 +195,6 @@
       color: #271c19;
     }
   }
-  .info-img{
-    text-align: center;
 
-  }
 
 </style>

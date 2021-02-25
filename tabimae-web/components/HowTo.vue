26eb34348d5adb89c6ee08418b4de88883a43bfe
wkn816 <wkn816@gmail.com>
2021-02-25 21:10:34 +0900

    update-titleの位置変更

--- tabimae-web/components/HowTo.vue
+++ tabimae-web/components/HowTo.vue
@@ -5,26 +5,25 @@
         <img :src="image_src" v-bind="attrs" v-on="on" class="howto-img">
       </template>
       <v-card>
-
-        <v-card-title class="howto-info-title">タビマエの使い方</v-card-title>
+        <p class="howto-info-title" text-center>タビマエの使い方</p>
         <v-divider></v-divider>
         <v-card-text>
           <ul class="howto-info-text">
-            <h2>はじめに</h2>
+            <h3>はじめに</h3>
             タビマエのご利用誠にありがとうございます。
             <li>当アプリは、新規会員登録していただくことで、いつどこでもご自身の旅行を確認すことができます。</li>
             <li>既に、新規会員登録されている方は「ログイン」から登録したメールアドレスとパスワードを入力して頂きご利用ください。</li>
             <li>タビマエをすぐお試し頂きたい方は「ゲストログイン」をご利用ください。</li>
-            <h2>タビ登録機能</h2>
+            <h3>タビ登録機能</h3>
             <li>お決まりのご旅行の・出発日・出発地/到着地・出発時間/到着時間・旅行のテーマを登録してください。</li>
             <li>旅行のテーマ（例）「家族旅行」「卒業旅行」「自分探し旅」など自由にテーマを設定できます！</li>
-            <h2>タビ確認機能</h2>
+            <h3>タビ確認機能</h3>
             <li>旅行一覧画面にて、「詳細」ボタンをクリックすると、より詳しく登録した旅行を確認できます。</li>
             <li>登録した旅行の・出発日・出発地/到着地・出発時間/到着時間・旅行のテーマを確認できます。</li>
             <li>登録したタビを確認することで、日付や場所、時間の間違いを防ぎます。</li>
             <li>出発までの残り日数を確認することができます。</li>
             <li>出発日が過ぎたタビは自動的に削除されます。</li>
-            <h2>その他機能</h2>
+            <h3>その他機能</h3>
             <li>出発までの残り日数に合わせた、メッセージが表示されます。</li>
             <li>メッセージを参考に旅行準備を進めましょう。</li>
             <li>忘れものリストに自由に持ち物を追加して、忘れ物を防ぎましょう。</li>
@@ -99,11 +98,22 @@
   .howto-info-title {
     color: #001858;
     text-align: center;
+    font-size: 30px;
+    @include sp {
+    font-size: 20px;
+    }
   }
 
   .howto-info-text {
     color: #001858;
     text-align: left;
   }
-
+  ul li{
+    padding-left: 1em;
+    text-indent: -1em;
+  }
+  h3{
+    padding-left: -1em;
+    text-indent: -1em;
+  }
 </style>

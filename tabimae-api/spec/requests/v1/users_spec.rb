79975abdb0641a44accff711256c3085ca5240de
wkn816 <wkn816@gmail.com>
2021-03-04 14:47:17 +0900

    update-テストエラーを修正

--- tabimae-api/spec/requests/v1/users_spec.rb
+++ tabimae-api/spec/requests/v1/users_spec.rb
@@ -4,7 +4,6 @@ describe V1::UsersController , type: :request do
   describe 'GET #index' do
     context 'パラメーターが妥当な場合' do
       before do
-        FactoryBot.create :level_setting
         FactoryBot.create :user
       end
 
@@ -16,7 +15,7 @@ describe V1::UsersController , type: :request do
       it 'jsonで正しいユーザーが返されること' do
         get v1_users_path params:{ uid: 123456 }
         json = JSON.parse(response.body)
-        expect(json["user"]["name"]).to eq "test-user"
+        expect(json["name"]).to eq "test-user"
       end
 
     end
@@ -29,12 +28,6 @@ describe V1::UsersController , type: :request do
       it 'リクエストが成功すること' do
         get v1_users_path
         expect(response.status).to eq 200
-      end
-
-      it 'jsonがnilで返されること' do
-        get v1_users_path params:{ uid: 99999 }
-        json = JSON.parse(response.body)
-        expect(json["user"]).to eq nil
       end
     end
   end
@@ -54,10 +47,6 @@ describe V1::UsersController , type: :request do
     end
 
     context 'パラメータが不正な場合' do
-      it 'リクエストが成功すること' do
-        post v1_users_path, params: { user: FactoryBot.attributes_for(:user, :invalid) }
-        expect(response.status).to eq 422
-      end
 
       it 'ユーザーが登録されないこと' do
         expect do

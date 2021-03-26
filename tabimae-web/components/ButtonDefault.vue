c0393262bd6bbd49c2ac4c38d4d65eb82b960671
wkn816 <wkn816@gmail.com>
2021-03-26 15:06:25 +0900

    update-不要な記述削除

--- tabimae-web/components/ButtonDefault.vue
+++ tabimae-web/components/ButtonDefault.vue
@@ -8,30 +8,28 @@
 </template>
 
 <script>
-  export default {}
-
+export default {};
 </script>
 
 <style lang="scss" scoped>
-  .button-default {
-    cursor: pointer;
-    display: block;
-    text-align: center;
-    // height: 150px;
-    border-radius: 8px;
-    border: 1px solid #dddddd;
+.button-default {
+  cursor: pointer;
+  display: block;
+  text-align: center;
+  // height: 150px;
+  border-radius: 8px;
+  border: 1px solid #dddddd;
 
-    &:hover {
-      transform: scale(1.04);
-      transition-duration: 40ms;
-    }
-
-    >p {
-      margin-top: 20px;
-      font-family: 'Noto Sans CJK JP';
-      font-size: 20px;
-      letter-spacing: 0.1em;
-    }
+  &:hover {
+    transform: scale(1.04);
+    transition-duration: 40ms;
   }
 
+  > p {
+    margin-top: 20px;
+    font-family: "Noto Sans CJK JP";
+    font-size: 20px;
+    letter-spacing: 0.1em;
+  }
+}
 </style>

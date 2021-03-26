36dd6316157952f951e4c0949a77b0141c262924
wkn816 <wkn816@gmail.com>
2021-03-26 15:08:25 +0900

    update-１クリックでゲストログイン可能にしました

---
+++ tabimae-web/components/GuestLoginButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="button-default">
+    <slot />
+    <p>
+      <slot name="label" />
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+.button-default {
+  cursor: pointer;
+  display: block;
+  text-align: center;
+  // height: 150px;
+  border-radius: 8px;
+  border: 1px solid #dddddd;
+
+
+  &:hover {
+    transform: scale(1.04);
+    transition-duration: 40ms;
+  }
+
+  > p {
+    margin-top: 20px;
+    font-family: "Noto Sans CJK JP";
+    font-size: 20px;
+    letter-spacing: 0.1em;
+  }
+}
+</style>

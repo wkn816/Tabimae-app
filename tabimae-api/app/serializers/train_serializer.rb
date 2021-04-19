71a953e7b3838465e2e8a4ab26e4a984e8d1bb24
wkn816 <wkn816@gmail.com>
2021-04-19 19:34:05 +0900

    fix-不要な記述削除

--- tabimae-api/app/serializers/train_serializer.rb
+++ tabimae-api/app/serializers/train_serializer.rb
@@ -1,8 +1,4 @@
 class TrainSerializer < ActiveModel::Serializer
   attributes :id, :departure_day, :departure_time, :arrival_time, :departure_place, :arrival_place, :user_id, :travel_id
   belongs_to :travel
-
-  # def username
-  #   object.user.name
-  # end
 end

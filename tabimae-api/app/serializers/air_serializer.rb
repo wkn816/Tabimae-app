316a21a169b4e510e261c29e500ff7c6be9f8df0
wkn816 <wkn816@gmail.com>
2021-04-19 19:33:59 +0900

    fix-不要な記述削除

--- tabimae-api/app/serializers/air_serializer.rb
+++ tabimae-api/app/serializers/air_serializer.rb
@@ -1,8 +1,4 @@
 class AirSerializer < ActiveModel::Serializer
   attributes :id, :departure_day, :departure_time, :arrival_time, :departure_place, :arrival_place, :flight_number, :airline, :user_id, :travel_id
   belongs_to :travel
-
-  # def username
-  #   object.user.name
-  # end
 end

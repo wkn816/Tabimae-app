3e92cd4f3dfbeae5d05d5518c1eb44ae3214e234
wkn816 <wkn816@gmail.com>
2021-06-17 20:01:00 +0900

    fix-一覧画面のテーブルのレスポンシブ

--- tabimae-web/pages/travelList.vue
+++ tabimae-web/pages/travelList.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
+  <v-app class="bg">
     <v-container>
       <v-row>
-        <v-col cols="12" sm="12" md="12" lg="12">
+        <v-col cols="12" offset-ms="1" sm="10" offset-md="1" md="11" offset-lg="1" lg="10">
           <v-card color="#ffc6c7" elevation="2" class="travellist-card">
             <v-card-title primary-title class="justify-center">
               <v-icon x-large color=#e53170>
@@ -20,7 +20,7 @@
 
     <v-container>
       <v-row>
-        <v-col cols="12" sm="11" md="11" lg="12">
+        <v-col cols="12" sm="11" offset-md="1" md="11" lg="10">
           <v-card elevation="10">
             <v-simple-table class="list-table" style="color:#001858">
               <thead class="list-thead">
@@ -61,7 +61,7 @@
       <TravelInfo />
     </v-container>
 
-  </div>
+  </v-app>
 </template>
 
 <script>

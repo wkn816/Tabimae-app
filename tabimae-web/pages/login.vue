1dd7548d33ed28b31c80e6315dce3428f8e2cd06
wkn816 <wkn816@gmail.com>
2021-06-19 01:32:21 +0900

    fix-ログイン画面ipadサイズ

--- tabimae-web/pages/login.vue
+++ tabimae-web/pages/login.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app class="bg">
     <v-row>
-      <v-col cols="12" sm="10" md="6" lg="6" offset-lg="3">
+      <v-col cols="12" lg="6" offset-lg="3">
         <v-card color=#8bd3dd class="login-card">
           <v-card-title primary-title class="justify-center">
             <v-icon x-large color=#e53170>
@@ -30,9 +30,9 @@
         </v-card>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col cols="12" sm="10" md="6" lg="6" offset-lg="3">
 
+    <v-row>
+      <v-col cols="12" lg="6" offset-lg="3">
         <v-card color=#f3d2c1>
           <v-card-title primary-title class="justify-center">
             <v-icon x-large color=#e53170>
